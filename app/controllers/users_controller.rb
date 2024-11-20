ad1b80ae91b36f7da8ff6d7dcce0e78bbbee3493
2211140070 <2211140070x@ed.fuk.kindai.ac.jp>
2024-11-20 08:52:00 +0000

    aaa

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    u = User.new(uid: params[:user][:uid],
+    password: params[:user][:password],
+    password_confirmation: params[:user][:password_confirmation])
+    u.save
+    redirect_to users_path
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :tweets, dependent: :destroy
+    has_many :likes, dependent: :destroy
+    has_many :like_tweets, through: :likes , source: :tweet
+    
+    has_secure_password
+    
+end
